d2b55b163fa498e4c6843baea6100cd81705919e
Muhammed Seyrek <91323889+muhammedSeyrek@users.noreply.github.com>
2022-01-13 11:51:43 +0300

    Update birSayisindanSayfaSayisi.c

--- birSayisindanSayfaSayisi.c
+++ birSayisindanSayfaSayisi.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-//689 tane 1 olan kitabýn sayfa sayisi nedir?
+//689 tane 1 olan kitabÄ±n sayfa sayisi nedir?
 int main(){
 	int kontrol=0,sayfaSayisi=1;
 	int sayi=1;
-	sayfaSayisi=sayi;
 	while(1){
 		sayi=sayfaSayisi;
 		while(sayi>0){  
