99f8692dc7f61d1666d93cb73423cd5408dec00b
Muhammed Seyrek <91323889+muhammedSeyrek@users.noreply.github.com>
2022-03-24 14:06:11 +0300

    Add files via upload
    This program returns the number of fibonacci in the desired sequence with the recursive algorithm.
    Bu program recursive algoritması ile istenen sıradaki fibonacci sayısını verir.

---
+++ fibonacciInRecursive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fibonacci();
+int main() {
+   int n = 6;
+   int i;
+   printf("Fibonacci of %d: " , n);
+   for(i = 0; i < n; i++) {
+      printf("%d ",fibonacci(i));            
+   }
+}
+int fibonacci(int n) {
+   	if(n == 0)
+      	return 0;
+    else if(n == 1) 
+      	return 1;
+    else
+		return (fibonacci(n-1) + fibonacci(n-2));
+   
+}
