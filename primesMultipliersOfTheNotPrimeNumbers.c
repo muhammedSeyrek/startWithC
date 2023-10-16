4e07ca5c4a8bcc972d2ee4bf2c3936267721288f
Muhammed Seyrek <91323889+muhammedSeyrek@users.noreply.github.com>
2023-10-16 20:12:51 +0300

    Update prime factorsOfNonPrime.c

--- primesMultipliersOfTheNotPrimeNumbers.c
+++ primesMultipliersOfTheNotPrimeNumbers.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
-int main(){
-	int i, j, k, flag;
-	for(i = 2; i < 100; i++){
-		flag = 1;
-		for(j = 2; j < i; j++){
+void main(void){
+	for(int i = 2; i < 100; i++){
+		int flag = 1;
+		for(int j = 2; j < i; j++)
 			if(i % j == 0){
-				if(flag == 1){
-					printf("%d = ", i);
-				}
+				if(flag == 1)
+					printf("%d : ", i);
 				flag = 0;
-				int flag2 = 0;
-				for(k = 2; k < j && flag2 == 0; k++){
-					if(j % k == 0){
-						flag2 = 1;
-					}
-				}
-				if(flag2 == 0){
+				int flag2 = 1;
+				for(int k = 2; k < j && flag2 == 1; k++)
+					if(j % k == 0)
+						flag2 = 0;
+				if(flag2 == 1)
 					printf("%d ", j);
-				}		
 			}
-			
-		}
-		if(flag == 0){
+		if(flag == 0)
 			printf("\n");
-		}
 	}
-	return 0;
 }
